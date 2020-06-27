a3dd0e87489838a3e1740bf871b8c865b4c03580
Anil Verma <anil.verma@e2open.com>
2020-06-27 16:13:50 +0530

    added generic support

--- HashTable.java
+++ HashTable.java
@@ -2,13 +2,13 @@
 import java.util.List;
 import java.util.ArrayList;
 
-public class HashTable {
+public class HashTable<A, B> {
 	
-	private List<Pair>[] arr = new ArrayList[100];
+	private List<Pair<A,B>>[] arr = new ArrayList[100];
 	
 	public static void main(String [] args) {
 		System.out.println("implementing hashtable");
-		HashTable table = new HashTable();
+		HashTable table = new HashTable<String, String>();
 		table.put("ashi", "Ashi Verma");
 		table.put("maahi", "Maahi Verma");
 		table.put("anil",  "Anil Verma");
@@ -16,33 +16,40 @@ public class HashTable {
         System.out.println("ashi->" + table.get("ashi"));
         System.out.println("maahi->"+ table.get("maahi"));
         System.out.println("anil->" + table.get("anil"));
-        System.out.println("anupam->" + table.get("anupam"));		
+        System.out.println("anupam->" + table.get("anupam"));	
+        System.out.println("raqndom->" + table.get("jhgeee")); 
+
+        HashTable table = new HashTable<Integer, String>();
+		table.put("ashi", "Ashi Verma");
+		table.put("maahi", "Maahi Verma");
+		table.put("anil",  "Anil Verma");
+		table.put("anupam", "Anupam Kaushal");		
 	}
 	
-	public String get(String key) {
+	public B get(A key) {
 		int hashCode = key.hashCode();
         int length = arr.length;
         int index = hashCode%length;
 		index = Math.abs(index);
-        List<Pair> itemList = arr[index];
+        List<Pair<A, B>> itemList = arr[index];
         if (itemList == null) {
 			return null;
         }
 		
         for (Pair item : itemList) {			 
             if (key.equals(item.getKey())) {
-				return item.getValue();
+				return (B)item.getValue();
 			}			
         }
         return null;		
 	}
 	
-	public void put(String key, String value) {		
+	public void put(A key, B value) {		
 		int hashCode = key.hashCode();
         int length = arr.length;
         int index = hashCode%length;
 		index = Math.abs(index);
-		List<Pair> itemList = arr[index];
+		List<Pair<A, B>> itemList = arr[index];
 		if (itemList == null) {
 			itemList = new ArrayList<>();
 			arr[index] = itemList;
@@ -50,7 +57,5 @@ public class HashTable {
 		Pair pair = new Pair(key, value);
 		itemList.add(pair);        		
 	}
-	
-	
 	
 }
